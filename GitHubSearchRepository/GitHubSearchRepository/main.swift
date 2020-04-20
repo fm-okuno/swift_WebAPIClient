b56f163c701e8b2358ac7c6996cf42573f54f190
fm-okuno <okuno@freemake.co.jp>
2020-04-20 15:46:13 +0900

    Xcodeにてプロジェクトを作成しました。 プロジェクト名「GitHubSearchRepository」

---
+++ GitHubSearchRepository/GitHubSearchRepository/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitHubSearchRepository
+//
+//  Created by 開発アカウント on 2020/04/20.
+//  Copyright © 2020 開発アカウント. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
